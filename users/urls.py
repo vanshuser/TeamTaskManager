e25007ea8882bfc6615a086091d5f3a5f2f82153
Vansh Sharma <[vanshsharma712005@gmail.com]>
2026-05-14 18:37:44 +0530

    Superuser

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import signup_view, login_view, logout_view, dashboard
+from .views import signup_view, login_view, logout_view, dashboard, create_admin
 urlpatterns = [
     path('', dashboard, name='home'),
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
+    path('create-admin/', create_admin),
 ]
